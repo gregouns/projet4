155499931eaa90898ccae6e2166cb46b20bdb26c
gregouns <greglelay@gmail.com>
2017-04-26 19:46:12 +0200

    add row

--- wp-content/themes/moto/templates/content.php
+++ wp-content/themes/moto/templates/content.php
@@ -3,10 +3,10 @@
 		<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 	</header>
 	<div class="row myarticle">
-		<div class="image col-md-4">
+		<div class="image col-md-3 col-sm-12">
 			<?php the_post_thumbnail('thumbnail'); ?>
 		</div>
-		<div class="entry-summary col-md-8">
+		<div class="entry-summary col-md-9 col-sm-12">
 			<?php the_excerpt(); ?>
 		</div>
 	</div>
